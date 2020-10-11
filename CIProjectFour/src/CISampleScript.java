22fb7e5baef2bfd5c4c975e771f2ff8675a66f73
sunil434 <sunilmichael90@gmail.com>
2020-10-11 13:20:15 +1100

    first version

---
+++ CIProjectFour/src/CISampleScript.java
@@ -0,0 +1,18 @@
+
+public class CISampleScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Selenium");
+		
+		System.out.println("Tosca");
+		
+		System.out.println("Sahi Pro");
+		
+		System.out.println("UFT");
+		
+
+	}
+
+}
